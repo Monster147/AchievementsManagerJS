c9f6dcc4e41cf2330067f6dbe0dd4ed67949312d
José Saldanha <jose.carapeto.saldanha@gmail.com>
2026-05-01 00:04:35 +0100

    Update start-server.bat

--- start-server.bat
+++ start-server.bat
@@ -1,9 +1,12 @@
 @echo off
-cd /d "C:\Project\1stVersionInJS"
+
+:: Go to the folder where this .bat file is located
+cd /d "%~dp0"
 
 :: Rewrite the log file (creates it or clears it)
 echo. > server.log
 
 echo Starting the server...
-:: Start the server and redirect both stdout and stderr to the log file
+
+:: Start the server and redirect stdout + stderr
 npm start >> server.log 2>&1
